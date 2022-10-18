657fbc0b51cc255c91358c333fefaeb727cb1fba
tudemaha <gede_mahardika88@yahoo.co.id>
2022-10-18 18:17:42 +0800

    add comments

--- dfa_minimalization/using_pandas/main.py
+++ dfa_minimalization/using_pandas/main.py
@@ -1,20 +1,27 @@
-from minimalization import *
+# import minimization module
+from minimalization import minimal
 
+# main function
 def __main__():
+    # program title
     print('===== DFA MINIMALIZATION =====')
 
+    # list of DFA machine
     print("Pilihan mesin:")
     print('1. Mesin DFA I')
     print('2. Mesin DFA II')
     print('3. Mesin DFA III')
 
+    # accpted user input
     accept = ('1', '2', '3')
+    # user input, repeat if empty
     while(True):
         print('Masukkan pilihan: ', end = '')
         choice = input()
         if choice in accept: break
         else: print('Masukan tidak valid\n')
 
+    # if user selects 1, minimize the first DFA machine
     if choice == '1':
         minimal('q0', {'q4'}, {'a', 'b'}, {
             ('q0', 'a'): 'q1',
@@ -28,6 +35,7 @@ def __main__():
             ('q4', 'a'): 'q1',
             ('q4', 'b'): 'q2'
         })
+    # if user selects 2, minimize the second DFA machine
     elif choice == '2':
         minimal('q0', {'q1', 'q2', 'q4'}, {'0', '1'}, {
             ('q0', '0'): 'q3',
@@ -43,6 +51,7 @@ def __main__():
             ('q5', '0'): 'q5',
             ('q5', '1'): 'q5',
         })
+    # if user selects 3, minimize the third DFA machine
     elif choice == '3':
          minimal('q0', {'q3', 'q5'}, {'0', '1'}, {
             ('q0', '0'): 'q1',
@@ -59,6 +68,8 @@ def __main__():
             ('q5', '1'): 'q5',
         })
 
+    # message to display program completed
     print('\n===== END OF PROGRAM =====')
 
+# run __main__ function
 __main__()
